435b0a70fd83f069f4e3646a9e979d5c8305b876
Chashita <chashita1019.becse24@chitkara.edu.in>
2025-04-28 19:16:20 +0530

    Update MusicQuiz.js

--- MusicQuiz.js
+++ MusicQuiz.js
@@ -1,6 +1,6 @@
 var currentSlide = 0;
 var slides = document.querySelectorAll('.slide');
-var totalSlides = slides.length;
+var totalSlides = slides.lengt;
 var nextButton = document.getElementById('next');
 nextButton.onclick = function() {
     if (currentSlide < totalSlides - 1) {
