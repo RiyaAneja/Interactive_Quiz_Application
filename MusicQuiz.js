6c4ca563e80a5d23fb5f85f51c5377762af1aef9
Riya <riya.aneja07@gmail.com>
2025-04-28 14:27:52 +0530

    Create MusicQuiz.js

---
+++ MusicQuiz.js
@@ -0,0 +1,33 @@
+var currentSlide = 0;
+var slides = document.querySelectorAll('.slide');
+var totalSlides = slides.length;
+var nextButton = document.getElementById('next');
+nextButton.onclick = function() {
+    if (currentSlide < totalSlides - 1) {
+        slides[currentSlide].classList.remove('active');
+        currentSlide++;
+        slides[currentSlide].classList.add('active');
+    } else {
+        var score = 0;
+        if (document.querySelector('input[name="q1"]:checked') && document.querySelector('input[name="q1"]:checked').value === 'C') {
+            score++;
+        }
+        if (document.querySelector('input[name="q2"]:checked') && document.querySelector('input[name="q2"]:checked').value === 'B') {
+            score++;
+        }
+        if (document.querySelector('input[name="q2"]:checked') && document.querySelector('input[name="q2"]:checked').value === 'C') {
+            score++;
+        }
+        if (document.querySelector('input[name="q3"]:checked') && document.querySelector('input[name="q3"]:checked').value === 'B') {
+            score++;
+        }
+        if (document.querySelector('input[name="q4"]:checked') && document.querySelector('input[name="q4"]:checked').value === 'C') {
+            score++;
+        }
+        if (document.querySelector('input[name="q5"]:checked') && document.querySelector('input[name="q5"]:checked').value === 'C') {
+            score++;
+        }
+        document.getElementById('result').innerText = 'You scored ' + score + ' out of ' + totalSlides;
+        nextButton.style.display = 'none';
+    }
+};
