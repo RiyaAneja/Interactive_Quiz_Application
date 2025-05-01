ece034b407dd1646cce16cdafc2829775cfaae96
Riya <riya.aneja07@gmail.com>
2025-05-02 01:27:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function getUsers() {
     const users = localStorage.getItem('users');
     return users ? JSON.parse(users) : [];
 }
+
 function saveUsers(users) {
     localStorage.setItem('users', JSON.stringify(users)); 
 }
